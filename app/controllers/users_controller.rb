cf8d8961927f305fef8897efb5595cec4bc676bd
bernardofcs <bernardofcs@hotmail.com>
2017-03-20 17:30:16 +0000

    added user email verification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,15 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    if User.find_by_email(user.email).present?
+      redirect_to '/signup', notice: "Email already taken."
     else
-      redirect_to '/signup'
+      if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        redirect_to '/signup'
+      end
     end
   end
 
